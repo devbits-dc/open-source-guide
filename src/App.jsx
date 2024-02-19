2c24f2ee445b6fbfd556ce18a8b557edc9df9286
Rituraj Jha <riturajreal01@gmail.com>
2024-02-19 10:38:44 +0530

    added my name

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <h1>Hii Enter your name here</h1>
+      <p>Rituraj Jha IT</p>
+    </>
+  )
+}
+
+export default App
